250a7c0f79d1eee4a724103118f3bfa8724b61f3
Daryl Ruggier <daryl.ruggier@gmail.com>
2022-12-15 11:37:18 +0100

    latest

--- vim-plug/mason-lspconfig.lua
+++ vim-plug/mason-lspconfig.lua
@@ -1,4 +1,4 @@
 require("mason-lspconfig").setup({
-    ensure_installed = { "sumneko_lua", "rust_analyzer", "typescript-language-server", "html-lsp", "css-lsp", "svelte-language-server", "haskell-language-server" },
+    ensure_installed = { "sumneko_lua", "rust_analyzer" },
     automatic_installation = true
 })
